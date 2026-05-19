8953e4b6c9a2e1ad8258a44d3fe3f8a7d6889fd8
Akshaya265 <akshayakatharla@gmail.com>
2026-05-19 23:17:50 +0530

    Add files via upload

---
+++ CodeAlpha_HangmanGame.py
@@ -0,0 +1,45 @@
+import random
+
+words = ["apple", "python", "house", "table", "river"]
+
+word = random.choice(words)
+
+guessed_letters = []
+wrong_guesses = 0
+max_wrong = 6
+
+print("===== HANGMAN GAME =====")
+
+while wrong_guesses < max_wrong:
+
+    display_word = ""
+
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+
+    print("\nWord:", display_word)
+
+    if "_" not in display_word:
+        print("Congratulations! You guessed the word:", word)
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("Already guessed!")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess not in word:
+        wrong_guesses += 1
+        print("Wrong guess!")
+        print("Remaining chances:", max_wrong - wrong_guesses)
+
+else:
+    print("Game Over!")
+    print("Correct word was:", word)
+    
---
+++ codeAlpha_Basicchatbot.py
@@ -0,0 +1,23 @@
+print("===== BASIC CHATBOT =====")
+print("Type 'bye' to exit")
+
+while True:
+
+    user = input("You: ").lower()
+
+    if user == "hello":
+        print("Bot: Hi!")
+
+    elif user == "how are you":
+        print("Bot: I'm fine, thank you!")
+
+    elif user == "what is your name":
+        print("Bot: I am a Python Chatbot")
+
+    elif user == "bye":
+        print("Bot: Goodbye!")
+        break
+
+    else:
+        print("Bot: Sorry, I don't understand")
+    
